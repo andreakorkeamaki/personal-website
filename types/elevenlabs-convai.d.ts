1facaea67db543f21090efc06eef4b72f631bcca
andreakorkeamaki <andreakorkeamaki@gmail.com>
2026-01-11 17:03:55 +0100

    Update ElevenLabs type definitions

--- types/elevenlabs-convai.d.ts
+++ types/elevenlabs-convai.d.ts
@@ -1,10 +1,16 @@
-declare namespace JSX {
-  interface IntrinsicElements {
-    "elevenlabs-convai": React.DetailedHTMLProps<
-      React.HTMLAttributes<HTMLElement>,
-      HTMLElement
-    > & {
-      "agent-id"?: string;
-    };
+import type * as React from "react";
+
+declare global {
+  namespace JSX {
+    interface IntrinsicElements {
+      "elevenlabs-convai": React.DetailedHTMLProps<
+        React.HTMLAttributes<HTMLElement>,
+        HTMLElement
+      > & {
+        "agent-id"?: string;
+      };
+    }
   }
 }
+
+export {};
